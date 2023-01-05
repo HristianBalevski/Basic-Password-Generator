a3716ce9ac7b4f1f6cad83f2916eb16b397d8339
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2023-01-05 12:15:33 +0000

    Update

--- Basic Password Generator.py
+++ Basic Password Generator.py
@@ -1,12 +1,14 @@
 import random
+import string
 
+punctuation_symbols = string.punctuation
 possible_symbols_for_the_password = []
 password = ''
 
-player = input('''Please Enter 'l' for Low password strength
-Please Enter 'm' for Medium password strength
-Please Enter 's' for Strong password strength
-Please Enter 'n' to exit the program: ''').lower()
+print('''Please Enter 'l' for Low password strength.
+Please Enter 'm' for Medium password strength.
+Please Enter 's' for Strong password strength.
+Please Enter 'n' to exit the program.''')
 
 # Here we add all digits(0-9) from ASCII Table
 for number in range(48, 58):
@@ -16,34 +18,29 @@ for number in range(48, 58):
 for lower_letter in range(97, 123):
     possible_symbols_for_the_password.append(chr(lower_letter))
 
-# Here we add the first part  special symbols from ASCII Table
-for first_symbols in range(33, 48):
-    possible_symbols_for_the_password.append(chr(first_symbols))
-
-# Here we add the second part special symbols from ASCII Table
-for second_symbols in range(58, 64):
-    possible_symbols_for_the_password.append(chr(second_symbols))
-
-# Here we add the third part special symbols from ASCII Table
-for third_symbols in range(91, 97):
-    possible_symbols_for_the_password.append(chr(third_symbols))
-
 # Here we add all upper letters from ASCII Table
 for upper_letter in range(65, 91):
     possible_symbols_for_the_password.append(chr(upper_letter))
 
-if player == 'l':
-    for i in range(1, 5):
-        password += random.choice(possible_symbols_for_the_password)
-elif player == 'm':
-    for i in range(1, 8):
-        password += random.choice(possible_symbols_for_the_password)
-elif player == 's':
-    for i in range(1, 11):
-        password += random.choice(possible_symbols_for_the_password)
-elif player == 'n':
-    exit()
-else:
-    print('Wrong input, please try again...')
-    exit()
-print(password)
+# Here we add all punctuation symbols
+for symbol in punctuation_symbols:
+    possible_symbols_for_the_password.append(symbol)
+
+while True:
+    player = input('Choose Strength: ').lower()
+    if player != 'l' and player != 'm' and player != 's' and player != 'n':
+        print('Wrong input, please try again...')
+        continue
+    elif player == 'l':
+        for i in range(1, 5):
+            password += random.choice(possible_symbols_for_the_password)
+    elif player == 'm':
+        for i in range(1, 8):
+            password += random.choice(possible_symbols_for_the_password)
+    elif player == 's':
+        for i in range(1, 16):
+            password += random.choice(possible_symbols_for_the_password)
+    elif player == 'n':
+        break
+    print(password)
+    break
